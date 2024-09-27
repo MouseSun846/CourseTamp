f1221d03c6f6a7f3807a58ea2a397a29bb167d09
mouse <3026098675@qq.com>
2024-09-27 22:21:04 +0800

    增加图标

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-cd ..
-cli publish --platform h5 --project CourseTamp 
-cd /d .\\CourseTamp
+@REM cd ..
+@REM cli publish --platform h5 --project CourseTamp 
+@REM cd /d .\\CourseTamp
 rd /q /s .\\docs 
 xcopy .\\unpackage\\dist\\build\\web\\* .\\docs\\  /E /I /H /Y 
