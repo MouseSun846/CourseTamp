8ce2ee7ba16cf87a8ffced4c7a657b446b1758bc
mouse <3026098675@qq.com>
2024-09-28 13:18:44 +0800

    ui 优化

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,13 +2,7 @@
 	<view class="container">
 		<view class="left-image">
 			<view class="image-content-2">
-				<view class="image-content-1">
-					<image class="img-body-1" mode="heightFix" src="/static/显微镜图标.svg" :draggable="false"></image>
-				</view>
-				<image class="img-body-2" mode="heightFix" src="/static/分组 2-左侧栏.svg" :draggable="false"></image>
-				<view class="image-content-3">
-					<image class="img-body-3" mode="heightFix" src="/static/13.svg" :draggable="false"></image>
-				</view>
+				<image class="img-body-2" mode="heightFix" src="/static/分组 12.svg" :draggable="false"></image>
 			</view>
 		</view>
 		<view class="content">
@@ -31,10 +25,7 @@
 
 		<view class="right-image">
 			<view class="right-image-content-1">
-				<image class="right-img-body-1" mode="heightFix" src="/static/分组 1-右侧栏.svg" :draggable="false"></image>
-				<view class="right-image-content-2">
-					<image class="right-img-body-2" mode="heightFix" src="/static/书本图标.svg" :draggable="false"></image>
-				</view>
+				<image class="right-img-body-1" mode="heightFix" src="/static/分组 11.svg" :draggable="false"></image>
 			</view>
 
 		</view>
@@ -139,19 +130,7 @@
 		pointer-events: none;
 	}
 
-	.image-content-1 {
-		position: absolute;
-		top: 9%;
-		left: 15%;
-		width: 30%;
-		height: 30%;
-		z-index: 2;
-	}
-
-	.img-body-1 {
-		height: 50%;
-		width: 5rem;
-	}
+	
 
 	.image-content-2 {
 		position: absolute;
@@ -164,19 +143,7 @@
 		height: 100%;
 	}
 
-	.image-content-3 {
-		position: absolute;
-		bottom: 0;
-		left: 10%;
-		width: 40%;
-		height: 8%;
-		z-index: 2;
-	}
-
-	.img-body-3 {
-		height: 60%;
-		width: 2rem;
-	}
+	
 
 	.right-image {
 		position: absolute;
@@ -212,20 +179,6 @@
 	.right-img-body-1 {
 		height: 100%;
 		width: 5rem;
-	}
-
-	.right-image-content-2 {
-		position: absolute;
-		bottom: 13%;
-		right: 22%;
-		width: 55%;
-		height: 20%;
-		z-index: 2;
-	}
-
-	.right-img-body-2 {
-		height: 50%;
-		width: 3rem;
 	}
 
 	.content {
