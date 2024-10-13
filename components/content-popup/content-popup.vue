8e68c0490e60c385e9d45b432d2dc84f34274bbc
mouse <3026098675@qq.com>
2024-10-13 11:11:31 +0800

    update

--- components/content-popup/content-popup.vue
+++ components/content-popup/content-popup.vue
@@ -14,28 +14,45 @@
 					</view>
 					<view class="content-wrap">
 
-						<!-- <view class="content-box">
+						<view class="content-box">
 							<scroll-view
 								:scroll-top="scrollTop"
 								scroll-y="true" 
 								:show-scrollbar="false"
 								class="scroll-Y" 
 								@scroll="scroll">
-								<uni-list-item
-										v-for="(item, index) in contentList"
-										:key="index"
-										@click="onItemClick(item, index)"
-										:class="{ 'selected': selectedIndex === index }"
-										:clickable="true">
-										<template v-slot:body>
-											<image class="item-choosing" mode="heightFix" src="/static/choosing.svg" :draggable="false" v-if="selectedIndex === index"></image>
-											<text class="slot-box slot-text">{{item}}</text>
-										</template>
-									</uni-list-item>
+
+									<uni-collapse ref="collapse" class="collapse-content" 
+									v-for="(contentItem, contentindex) in contentList"
+									:key="contentindex">
+
+										<uni-collapse-item title-border="none" :border="false">
+											<template v-slot:title>
+												<view class="content-title-box"> 
+													<svg t="1728784136346" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="13259" width="25" height="25"><path d="M998.817433 472.690256l-171.015748 173.005626c-7.33412 7.33412-12.223533 19.500798-9.778826 29.222771l41.503157 248.564112c7.390973 34.112184-7.277266 65.779661-34.169038 85.28046a88.236849 88.236849 0 0 1-48.89413 14.611385c-12.166679 0-26.834918-2.444707-39.05845-9.721972L524.885493 896.590994a22.229773 22.229773 0 0 0-24.447065 0L287.976377 1013.709491a87.384044 87.384044 0 0 1-87.952581-4.889413c-29.279624-19.500798-41.503157-51.168276-36.613744-85.280459l41.503157-246.119406c2.444707-12.166679 0-21.888651-7.334119-29.222771L24.175489 472.690256A93.183115 93.183115 0 0 1 4.504129 382.634091c9.778826-29.279624 34.169037-51.168276 65.950222-56.057688L307.477175 289.962659c9.778826 0 17.056092-7.33412 21.945505-17.056092L434.488206 48.732666A82.039802 82.039802 0 0 1 510.274108 0.009097c31.781185 0 61.060809 19.500798 75.729047 48.723569l107.453379 224.173901a31.042087 31.042087 0 0 0 22.002359 17.056092l236.96597 36.55689c29.336478 2.444707 56.22825 24.333358 65.950222 56.057689 12.223533 31.667477 2.444707 65.779661-19.500798 90.113018z" fill="#FFC83C" p-id="13260"></path></svg>
+												<text class="text">{{ contentItem.title }}</text>
+												</view>
+											</template>
+											<view class="content-text-box">
+												<view class="content-text"
+													v-for="(levelItem, levelIndex) in contentItem.levelInfoList"
+													:key="levelIndex">
+														<view class="level-number-box"> 
+															<view class="level-number"> 
+																<text class="level-number-text">{{ levelItem.levelNumber }}</text>
+															</view>
+														</view>
+														<text class="content-text-wrapper">{{ levelItem.levelTitle }}</text>
+													</view>
+											</view>
+
+										</uni-collapse-item>
+									
+									</uni-collapse>									
 
 							</scroll-view>	
 
-						</view> -->
+						</view>
 					</view>
 				</view>
 
@@ -54,18 +71,63 @@
 			return {
 					scrollTop: 0,
 					selectedIndex: null, 
-					contentList:[
-						'列表文字列表文字1列表文字列表文字',
-						'列表文字2',
-						'列表文字3',
-						'列表文字4',
-						'列表文字5',
-						'列表文字1',
-						'列表文字2',
-						'列表文字3',
-						'列表文字4',
-						'列表文字5'
+					contentList:[],
+					contents:{
+						commonLevel :[
+						{
+							title: "文件领学1：辅导员的定义与工作职责",
+							levelInfoList:[ 
+								{levelNumber: 1, levelTitle: "43号令_《普通高等学校辅导员队伍建设规定》"},
+								{levelNumber: 2, levelTitle: "2号令_《高等学校辅导员队伍建设规定》"},
+								{levelNumber: 3, levelTitle: "3号令_《高等学校辅导员队伍建设规定》"},
+							]
+						},
+						{
+							title: "文件领学2：思想理论教育和价值引领",
+							levelInfoList:[ 
+								{levelNumber: 1, levelTitle: "16号令_《关于进一步加强和改进大学生思想政治教育的意见》"},
+								{levelNumber: 2, levelTitle: "《习近平总书记在全国高校思想政治工作会议上的讲话》"},
+								{levelNumber: 3, levelTitle: "31号令_《关于加强和改进新形势下高校思想政治工作的意见》"},
+								{levelNumber:4, levelTitle: "62号令_《高校思想政治工作质量提升工程实施纲要》"}
+							]
+						},
+						{
+							title: "文件领学1：辅导员的定义与工作职责",
+							levelInfoList:[ 
+								{levelNumber: 1, levelTitle: "43号令_《普通高等学校辅导员队伍建设规定》"},
+								{levelNumber: 2, levelTitle: "2号令_《高等学校辅导员队伍建设规定》"},
+								{levelNumber: 3, levelTitle: "3号令_《高等学校辅导员队伍建设规定》"},
+							]
+						},
+						{
+							title: "文件领学1：辅导员的定义与工作职责",
+							levelInfoList:[ 
+								{levelNumber: 1, levelTitle: "43号令_《普通高等学校辅导员队伍建设规定》"},
+								{levelNumber: 2, levelTitle: "2号令_《高等学校辅导员队伍建设规定》"},
+								{levelNumber: 3, levelTitle: "3号令_《高等学校辅导员队伍建设规定》"},
+							]
+						}
 					],
+					hotLevel: [
+							{
+								title: "文件领学1：辅导员的定义与工作职责",
+								levelInfoList:[ 
+									{levelNumber: 1, levelTitle: "43号令_《普通高等学校辅导员队伍建设规定》"},
+									{levelNumber: 2, levelTitle: "2号令_《高等学校辅导员队伍建设规定》"},
+									{levelNumber: 3, levelTitle: "3号令_《高等学校辅导员队伍建设规定》"},
+								]
+							},
+							{
+								title: "文件领学2：思想理论教育和价值引领",
+								levelInfoList:[ 
+									{levelNumber: 1, levelTitle: "16号令_《关于进一步加强和改进大学生思想政治教育的意见》"},
+									{levelNumber: 2, levelTitle: "《习近平总书记在全国高校思想政治工作会议上的讲话》"},
+									{levelNumber: 3, levelTitle: "31号令_《关于加强和改进新形势下高校思想政治工作的意见》"},
+									{levelNumber:4, levelTitle: "62号令_《高校思想政治工作质量提升工程实施纲要》"}
+								]
+							},
+						]
+					},
 					activeIndex: 0,
 					tabs: ['普通关卡', '热点关卡'] // Tab内容数组
 			}
@@ -74,6 +136,8 @@
 			eventBus.on("show-content-popup", (e) => {
 				this.showContentDialog()
 			})
+			// 初始化目录列表书籍显示
+			this.onTabClick(0)
 		},
 		methods: {
 			// 显示目录popup 
@@ -94,6 +158,13 @@
 			
 			onTabClick(index) {
 				this.activeIndex = index
+				if(this.activeIndex === 0) {
+					// 普通关卡
+					this.contentList = this.contents.commonLevel
+				} else if(this.activeIndex ===1) {
+					// 热点关卡
+					this.contentList = this.contents.hotLevel
+				}
 			}
 		}
 	}
@@ -124,7 +195,7 @@
 	
 	.scroll-Y{
 		position: absolute;
-		height: 300px;
+		height: 250px;
 		width: 90%;
 	}
 	
@@ -226,6 +297,65 @@
 	  border-radius: 0px 0px 10px 10px;
     }
 
-
-
+	.content-box {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+	.collapse-content {
+		margin-top: 20px;
+	}
+	.content-title-box {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+	.text {
+		margin-left: 8px;
+		font-family: 'system-ui';
+		font-weight: bolder;
+		font-size: 16px;
+	}
+	.level-number-box {
+		width: 25px;
+		height: 25px;
+		margin-right: 8px;
+	}
+	.level-number {
+		width: 25px;
+		height: 25px;
+		border-radius: 50%;
+		background: #ffc83c;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+	.level-number-text {
+		color: #ffffff;
+		text-align: center;
+		line-height: 25px;
+		font-size: 12px;
+	}
+	.content-text-box {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+	}
+	.content-text {
+		margin-left: 20px;
+		padding: 10px;
+		font-family: 'system-ui';
+		font-weight: bolder;
+		font-size: 15px;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		width: 100%;
+	}
+	.content-text-wrapper {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		margin-right: 30px;
+	}
 </style>
