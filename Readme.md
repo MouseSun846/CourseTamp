f04bb5c2a2408dd01146334ef81d86b69b25dace
mousesun <3026098675@qq.com>
2024-10-18 10:23:29 +0800

    update

--- Readme.md
+++ Readme.md
@@ -1,2 +1,20 @@
 # 在线配色
 [https://gradients.app/zh/mesh](https://gradients.app/zh/mesh)
+
+
+# 波纹生成
+https://svgwave.in/
+
+[好玩的 CSS - 40 个有趣的 CSS 学习网站](https://juejin.cn/post/7375532797383573513)
+
+https://segmentfault.com/a/1190000041164613
+
+
+https://color.oulu.me/
+
+
+
+```
+停止winnat服务：net stop winnat
+启动winnat服务：net start winnat
+```
--- pages/user-center/user-center.vue
+++ pages/user-center/user-center.vue
@@ -17,15 +17,19 @@
 							<view class="book-box book-box-view book-box-view-bg" v-if="bookList.length === 0">
 								<view class="no-bind-book">
 									<svg t="1728999818523" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1572" width="32" height="32" style="margin-left: 10px; margin-right: 20px;"><path d="M661.76 65.28c-64 0-119.04 38.4-149.76 94.72-30.08-56.32-85.76-94.72-149.76-94.72H65.92v835.84h272.64c51.2 0 135.68 17.28 149.12 57.6 12.8 0 19.84 0.64 49.28 0v-0.64c13.44-40.32 97.92-57.536 148.48-57.536h272.64V65.28h-296.32zM338.56 843.52H115.84V122.88h247.68c46.72 0 94.08 27.52 113.92 70.4v679.872c-30.208-29.248-95.424-30.784-138.88-29.632z m570.24-0.64h-223.36c-43.456-1.152-110.144 0.512-139.456 30.528V193.28h1.216c19.84-42.88 67.2-70.4 113.92-70.4h247.68v720z" fill="#8492a6" p-id="1573"></path></svg>
-									<text class="book-text">{{ bookNumber||'未绑定' }}</text>
+									<text class="book-text">{{'未绑定' }}</text>
 								</view>
 							</view>
 	
 							<view class="book-box book-box-view" v-if="bookList.length">
 								<view class="book-box-body" v-for="(item, index) in bookList" :key="index">
+								
 									<view class="book-box-body-content">
-										<view class="book-box-body-number">
-											<svg t="1729172555273" class="icon" viewBox="0 0 1068 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4494" width="32" height="32"><path d="M999.513043 44.521739h-489.73913l-48.973913 48.973913v881.530435l48.973913 48.973913h489.73913l48.973914-48.973913V93.495652L999.513043 44.521739zM558.747826 142.469565h146.921739v783.582609h-146.921739V142.469565z m391.791304 783.582609h-146.921739V142.469565h146.921739v783.582609z" fill="#f2ff7f" p-id="4495"></path><path d="M158.052174 20.034783l-53.426087 44.521739L20.034783 950.53913l44.521739 53.426087 258.226087 20.034783 53.426087-44.521739 86.817391-885.982609-44.521739-53.426087L158.052174 20.034783z m124.660869 901.565217l-160.27826-13.356522L198.121739 122.434783l160.278261 11.130434-75.686957 788.034783z m545.391305-656.695652h97.947826v171.408695h-97.947826z m-244.869565 0h97.947826v171.408695h-97.947826z" fill="#f2ff7f" p-id="4496"></path></svg>
+										<view class="book-operate" @click="onBookDelete">
+											<svg t="1729215285785" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4455" width="30" height="30"><path d="M512 512m-512 0a512 512 0 1 0 1024 0 512 512 0 1 0-1024 0Z" fill="#E6E9FC" p-id="4456"></path><path d="M686.933333 332.8c8.533333 0 21.333333 0 25.6 4.266667 8.533333 4.266667 12.8 8.533333 17.066667 12.8 4.266667 4.266667 8.533333 12.8 8.533333 17.066666 0 4.266667 4.266667 12.8 4.266667 17.066667V396.8h-34.133333v277.333333c0 8.533333 0 12.8-4.266667 21.333334s-8.533333 12.8-12.8 17.066666c-4.266667 4.266667-12.8 8.533333-21.333333 12.8s-17.066667 4.266667-25.6 4.266667H409.6c-8.533333 0-17.066667 0-25.6-4.266667-8.533333-4.266667-17.066667-8.533333-21.333333-12.8-4.266667-4.266667-8.533333-12.8-12.8-17.066666s-8.533333-17.066667-8.533334-25.6V396.8h-29.866666v-4.266667-17.066666c0-4.266667 0-8.533333 4.266666-17.066667s4.266667-12.8 8.533334-17.066667 8.533333 0 12.8-4.266666 12.8-4.266667 25.6-4.266667h46.933333V298.666667c0-8.533333 4.266667-17.066667 8.533333-21.333334 4.266667-8.533333 12.8-8.533333 21.333334-8.533333h166.4c12.8 0 21.333333 4.266667 25.6 8.533333 4.266667 4.266667 8.533333 12.8 8.533333 21.333334v34.133333h46.933333z m-247.466666 0h166.4V298.666667h-166.4v34.133333z m-12.8 332.8c12.8 0 17.066667-8.533333 17.066666-21.333333V401.066667h-34.133333v243.2c0 8.533333 0 12.8 4.266667 17.066666 0 4.266667 4.266667 4.266667 12.8 4.266667z m98.133333 0c4.266667 0 8.533333 0 12.8-4.266667s4.266667-8.533333 4.266667-17.066666V401.066667h-34.133334v243.2c0 12.8 4.266667 21.333333 17.066667 21.333333z m98.133333 0c4.266667 0 8.533333 0 12.8-4.266667 0-4.266667 4.266667-8.533333 4.266667-17.066666V401.066667h-34.133333v243.2c0 12.8 4.266667 21.333333 17.066666 21.333333z" fill="#335ABE" p-id="4457"></path></svg>
+										</view>
+										<view class="book-tag">
+											编号
 										</view>
 									</view>
 									<view class="book-box-body-title-container">
@@ -88,6 +92,12 @@
 		</scroll-view>	
 		</view>
 		
+		<view>
+			<!-- 输入框示例 -->
+			<uni-popup ref="bookOperateDialog" type="dialog">
+				<uni-popup-dialog type="info" mode="base" title="是否确认解绑?" @confirm="bookOperateDialogConfirm"></uni-popup-dialog>
+			</uni-popup>
+		</view>
 	</view>
 </template>
 
@@ -116,8 +126,7 @@
 						number: 3,
 						predictGoal: 30
 					},
-					bookList: ["No.123456", "No.123456" , "No.123456" , "No.123456" , "No.1234562222222222", "No.123456", "No.123456" , "No.123456" , "No.123456" , "No.1234562222222222"],
-					bookNumber: '',
+					bookList: ["No.123456"],
 					bookNumberValue: '',
 					goalNumber: 0,
 					goalNumberValue: 0,
@@ -136,7 +145,7 @@
 				this.goalNumber = this.goalNumberValue
 			},
 			onBookNumberBind() {
-				this.bookNumber = this.bookNumberValue
+				this.bookList.push(this.bookNumberValue)
 			},
 		   getStepContentClass(isLocked) {
 			  return isLocked ? 'step-content-body step-content-body-lock' : 'step-content-body step-content-body-unlock';
@@ -147,6 +156,12 @@
 			},
 			onbookNumberIuput(e) {
 				console.log(e)
+			},
+			onBookDelete() {
+				this.$refs.bookOperateDialog.open()
+			},
+			bookOperateDialogConfirm() {
+				console.log('confirm unbind book')
 			}
 		}
 	}
@@ -253,20 +268,44 @@
 		font-size: 17px;
 	}
 	.book-box-body {
-		width: 88px;
-		height: 88px;
-		background:  #63cafe;
+		width: 120px;
+		height: 90px;
+		background:  #f27b14d1;
 		border-radius: 20px;
 		display: flex;
 		flex-direction: column;
 		margin-left: 10px;
 		margin-top: 10px;
+		overflow: hidden;
+	}
+
+	.book-tag{
+		position: relative;
+		top:8px;
+		right:-20px;
+		width:80px;
+		height:20px;
+		line-height:20px;
+		transform:rotate(45deg);
+		text-align:center;
+		background:#f00;
+		color:#fff;
+		opacity:0.7;
+		border-bottom:solid 1px #666;box-shadow:0 0 4px 2px rgb(0 0 0 / 35%);
 	}
 	.book-box-body-content{
 		flex: 4;
 		display: flex;
 		flex-direction: row;
-		justify-content: center;
+		justify-content: flex-end;
+
+	}
+	.book-operate {
+		width: 30px;
+		height: 30px;
+		margin-top: 3px;
+		margin-left: -10px;
+		cursor: pointer;
 	}
 	.book-box-body-title-container {
 		flex: 6;
@@ -275,13 +314,14 @@
 		justify-content: center;
 	}
 	.book-box-body-title {
-		width: 90%;
-		height: 100%;
+		width: 70%;
+		height: auto;
 		color: #ffffff;
 		word-wrap: break-word; /* 确保长单词可以换行 */
 		overflow-wrap: break-word; /* 兼容性更好 */
 		text-align: center;
 		padding-top: 10px;
+
 	}
 
 	.book-box-body-number {
